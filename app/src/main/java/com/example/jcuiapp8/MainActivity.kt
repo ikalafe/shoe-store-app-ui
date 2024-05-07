d3d8974105703b4964a0c8c36c3b55d23dba2a39
Daniyal <daniyaldehghan2021@gmail.com>
2024-05-07 21:58:55 +0330

    add composable Top Header

--- app/src/main/java/com/example/jcuiapp8/MainActivity.kt
+++ app/src/main/java/com/example/jcuiapp8/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jcuiapp8.ui.theme.AppDark
 import com.example.jcuiapp8.ui.theme.JCUIApp8Theme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +44,39 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainView(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(AppDark)
+    ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            TopHeader()
+        }
+    }
+}
 
+@Composable
+fun TopHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(25.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo",
+            modifier = Modifier.size(40.dp),
+            tint = Color.White
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.menu_dots),
+            contentDescription = "Menu",
+            modifier = Modifier.size(30.dp),
+            tint = Color.White
+        )
+    }
 }
 
 @Preview(showBackground = true)
