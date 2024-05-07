99b4ed2dd5db88b69ac5a64340f30dfea2a22925
Daniyal <daniyaldehghan2021@gmail.com>
2024-05-08 00:03:51 +0330

    crate composable function ProductBox

--- app/src/main/java/com/example/jcuiapp8/MainActivity.kt
+++ app/src/main/java/com/example/jcuiapp8/MainActivity.kt
@@ -4,24 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -29,8 +46,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.jcuiapp8.data.MockData
 import com.example.jcuiapp8.ui.theme.AppDark
 import com.example.jcuiapp8.ui.theme.AppLight
+import com.example.jcuiapp8.ui.theme.AppRed
 import com.example.jcuiapp8.ui.theme.JCUIApp8Theme
 
 class MainActivity : ComponentActivity() {
@@ -49,6 +68,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainView(modifier: Modifier = Modifier) {
     Box(
@@ -58,7 +78,135 @@ fun MainView(modifier: Modifier = Modifier) {
     ) {
         Column(modifier = Modifier.fillMaxSize()) {
             TopHeader()
+            Text(
+                text = "Collections",
+                color = AppLight,
+                fontSize = 28.sp,
+                textAlign = TextAlign.End,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(horizontal = 25.dp)
+            )
+            ProductBox()
+        }
+    }
+}
 
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun ProductBox() {
+    LazyVerticalGrid(columns = GridCells.Fixed(2), contentPadding = PaddingValues(10.dp)) {
+        items(MockData.list.size) {
+            val item = MockData.list[it]
+            var liked by remember { mutableStateOf(item.liked) }
+            Card(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxSize(),
+                colors = CardDefaults.cardColors(Color.Transparent),
+                shape = RoundedCornerShape(25.dp)
+            ) {
+                Column {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(230.dp)
+                            .background(Color.Transparent)
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(170.dp)
+                                .padding(15.dp)
+                                .clip(RoundedCornerShape(25.dp))
+                                .align(Alignment.BottomCenter)
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .background(AppLight)
+                                    .padding(15.dp)
+                            ) {
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .align(Alignment.BottomCenter),
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.SpaceBetween
+                                ) {
+                                    Row {
+                                        Icon(
+                                            painter = painterResource(id = R.drawable.star),
+                                            contentDescription = "Rate",
+                                            tint = Color.White,
+                                            modifier = Modifier.size(15.dp)
+                                        )
+                                        Spacer(modifier = Modifier.width(5.dp))
+                                        Text(
+                                            text = item.rate.toString(),
+                                            fontSize = 12.sp,
+                                            color = Color.White
+                                        )
+                                    }
+                                    IconButton(
+                                        onClick = { liked = !liked },
+                                        modifier = Modifier.size(28.dp)
+                                    ) {
+                                        Icon(
+                                            painter = painterResource(id = if (liked) R.drawable.heart_fill else R.drawable.heart_empty),
+                                            contentDescription = "Heart",
+                                            tint = if (liked) AppRed else Color.White,
+                                            modifier = Modifier
+                                                .size(40.dp)
+                                                .padding(5.dp)
+                                                .clip(
+                                                    RoundedCornerShape(5.dp)
+                                                )
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(160.dp)
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .width(70.dp)
+                                    .height(40.dp)
+                                    .align(Alignment.BottomCenter)
+                                    .shadow(
+                                        15.dp,
+                                        CircleShape
+                                    )
+                            )
+                            Image(
+                                painter = painterResource(id = item.image),
+                                contentDescription = item.name,
+                                modifier = Modifier
+                                    .width(160.dp)
+                                    .rotate(45f)
+                                    .align(Alignment.TopCenter)
+                                    .padding(top = 65.dp, start = 25.dp)
+                            )
+                        }
+                    }
+                    Text(
+                        text = item.name,
+                        color = Color.White,
+                        fontSize = 16.sp,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Text(
+                        text = "${item.price}$", color = AppLight,
+                        fontSize = 14.sp,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            }
         }
     }
 }
